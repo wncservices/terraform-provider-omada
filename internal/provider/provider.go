efa593d6e783740f2e5aa83219a6dacf9193ea05
wilant <wilant.nackaerts@datacamp.com>
2026-07-26 21:36:06 +0200

    wip: switch port

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -161,6 +161,7 @@ func (p *OmadaProvider) Resources(_ context.Context) []func() resource.Resource 
 		NewSessionLimitResource,
 		NewGatewayBandwidthControlResource,
 		NewPortalAccessControlResource,
+		NewSwitchPortResource,
 		NewSSHSettingsResource,
 		NewDot1XResource,
 		NewMACAuthResource,
